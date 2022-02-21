a7f94e9884e092a97595523e6b60cd8da13a0533
VovaTryfonov <vtryfonov99@gmail.com>
2022-02-21 17:08:12 +0200

    test

---
+++ app.js
@@ -0,0 +1,10 @@
+/*Slider https://kenwheeler.github.io/slick/*/
+$('.slider__js').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+});
